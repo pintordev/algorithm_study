e15f0600c3efa9f0945192324c554cb2825d4231
pintor.dev <pintor.dev@gmail.com>
2025-06-22 15:37:30 +0900

    level #1 12931

---
+++ src/main/java/base/solution_number/Solution_number.java
@@ -0,0 +1,8 @@
+package base.solution_number;
+
+class Solution_number {
+    public static void main(String[] args) {
+
+        Solution_number _s = new Solution_number();
+    }
+}
--- src/main/java/solution_number/Solution_number.java
+++
@@ -1,9 +0,0 @@
-package solution_number;
-
-class Solution_number {
-
-    public static void main(String[] args) {
-
-        Solution_number s = new Solution_number();
-    }
-}
